2b98ae4aa78f2817c7e9268f6958b9ccb81f1b76
siva-sms <149375132+siva-sms@users.noreply.github.com>
2023-11-10 11:05:19 +0530

    Update test_user_model.py

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -23,13 +23,13 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password='wolf')
+        u = User(password='cat')
         with self.assertRaises(AttributeError):
             u.password
 
     def test_password_verification(self):
         u = User(password='cat')
-        self.assertTrue(u.verify_password('eagle'))
+        self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
